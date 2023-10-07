4041175e34434478ee8e083b2fd323656fde3f89
Mdajaj123 <137004218+Mdajaj123@users.noreply.github.com>
2023-10-07 23:55:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  return num.toString(2);
 }
+let decimalnumber1=7;
+let binarynumber1=decimalToBinary(decimalnumber1);
+console.log(binarynumber1)
+
+let decimalnumber2=10;
+let binarynumber2=decimalToBinary(decimalnumber2);
+console.log(binarynumber2)
+let decimalnumber3=33;
+let binarynumber3=decimalToBinary(decimalnumber3);
+console.log(binarynumber3)
 
 window.decimalToBinary = decimalToBinary;
